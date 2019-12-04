873cbbf84bf56586a2db358bad00876f5c52c472
Itay Levy <itaylevy134@gmail.com>
2019-12-04 21:04:24 +0200

    finished ssh-copy-id betwween the hosts

--- answers.sh
+++ answers.sh
@@ -4,6 +4,8 @@ apt install ansible
 # Create the ansible user on both the control host and the workstation host.
 # The ansible user get created with a home directory (-m) sudo privileges (-G sudo) and bash as the default shell (-s which bash).
 # On the google cloud vm the sudo group wasn't exist so i should add ansible enrty via visudo.
+# ansible  ALL=(ALL) NOPASSWD: ALL
 sudo useradd -m -s $(which bash) -G sudo ansible
 sudo passwd ansible
 
+# Copy the ~/.ssh/id_rsa.pub content from the control host and to append it to the end of the ~/.ssh/authorized_keys file in the client host
